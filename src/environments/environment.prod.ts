dc118948ca688187ed34b2cd58a6e3732d808564
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-01-27 10:04:09 +0100

    Install new dependencies, create client interface, set environment, update clients with data from firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCB1vSAdbt2lNJV1S5qtiDKreTY6ur1tHY",
+  authDomain: "clientpanelprod-8adea.firebaseapp.com",
+  databaseURL: "https://clientpanelprod-8adea.firebaseio.com",
+  projectId: "clientpanelprod-8adea",
+  storageBucket: "clientpanelprod-8adea.appspot.com",
+  messagingSenderId: "768587898397"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCB1vSAdbt2lNJV1S5qtiDKreTY6ur1tHY",
+  authDomain: "clientpanelprod-8adea.firebaseapp.com",
+  databaseURL: "https://clientpanelprod-8adea.firebaseio.com",
+  projectId: "clientpanelprod-8adea",
+  storageBucket: "clientpanelprod-8adea.appspot.com",
+  messagingSenderId: "768587898397"
+  }
 };
 
 /*
