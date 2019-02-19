6ef6a4cc177afee847693710ddd1f50c5e1e0efc
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-02-19 17:42:26 +0100

    Create settings, register, guards and setting.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,22 +11,25 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 //import { SidebarComponent } from './components/sidebar/sidebar.component';
 //import { ClientsComponent } from './components/clients/clients.component';
 import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AuthGuard } from './guards/auth.guard';
+import { RegisterGuard } from './guards/register.guard';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent, canActivate:[AuthGuard]},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'client/add', component: AddClientComponent},
-  {path: 'client/edit/:id', component: EditClientComponent},
-  {path: 'client/:id', component: ClientDetailsComponent},
-  {path: 'settings', component: SettingsComponent},
+  {path: 'register', component: RegisterComponent, canActivate:[RegisterGuard]},
+  {path: 'client/add', component: AddClientComponent, canActivate:[AuthGuard]},
+  {path: 'client/edit/:id', component: EditClientComponent, canActivate:[AuthGuard]},
+  {path: 'client/:id', component: ClientDetailsComponent, canActivate:[AuthGuard]},
+  {path: 'settings', component: SettingsComponent, canActivate:[AuthGuard]},
   {path: '**', component: NotFoundComponent},
 ];
 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[AuthGuard, RegisterGuard]
 })
 export class AppRoutingModule { 
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -20,4 +20,16 @@ export class AuthService {
   getAuth() {
     return this.afAuth.authState.map(auth => auth);
   }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  register(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then(userData => resolve(userData), 
+        err => reject(err))
+    })
+  }
 }
